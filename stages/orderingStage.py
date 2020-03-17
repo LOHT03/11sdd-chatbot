f64ee1e75220cfd530beb3a3fed0244523f702ba
Jared Lohtaja <LOHT03@stpiusx.nsw.edu.au>
2020-03-17 11:52:52 +1100

    Added orderingStage documentation

--- stages/orderingStage.py
+++ stages/orderingStage.py
@@ -10,77 +10,146 @@ def orderingStage(customer: Customer, existingOrder: Order = None):
     existing order for recursion.
     """
 
-    # Check if an order was provided as a paramater
+    # Check if an order was provided as a paramater by checking if the order
+    # is an instance of an order
     customerOrder: Order = existingOrder if isinstance(
         existingOrder, Order) else Order()
-    isOrdering = True
-    while isOrdering:
+
+    while True:
+        # Ask the customer what course they would like to order from
         option = input(
             f"What course would you like to order from? {menuInstance.menu.keys()}: ")
+
+        # Check if the customer provided an input
         if len(option) != 0:
+
+            # Fuzzy process user input for a valid course from the menu
             (selectedCourse, confidence) = process.extractOne(
                 option, menuInstance.menu.keys())
+
+            # Check if the package is more than 80% confident
             if confidence >= 80:
-                menuLoop = True
-                while menuLoop:
+
+                while True:
+
+                    # Ask the user if they would like to see a menu or to enter
+                    # a meal they would like to order
                     selectedDishInput = input(
                         "What meal would you like? (Or would you like to see a menu?): ")
+
+                    # Exit loop if they don't enter anything
                     if len(selectedDishInput) == 0:
-                        menuLoop = False
+                        break
+
+                    # If the customer did enter something
                     else:
+
+                        # Fuzzy process user input for a meal valid meal or to see a menu
                         (selectedDish, selectedDishConfidence) = process.extractOne(
                             selectedDishInput, ["see a menu", *list(menuInstance.menu[selectedCourse].keys())])
+
+                        # If fuzzy is more than 80% confident
                         if selectedDishConfidence >= 80:
+
+                            # If user wishes to see a menu
                             if selectedDish == "see a menu":
+
+                                # Show the menu for the specified course
                                 menuInstance.show(selectedCourse)
                             else:
+
+                                # Add the item that the customer entered to the
+                                # order
                                 customerOrder.addItem(
                                     menuInstance.getDish(selectedDish))
+
+        # No input was entered
         else:
             print()
+
+            # Show the current order to the customer
             customerOrder.showTotal()
-            goingtoConfirm = True
-            while goingtoConfirm:
+
+            while True:
+
+                # If the order is valid
                 if customerOrder.isValid:
                     print("You have enough items for a delivery.")
+
+                    # Ask the customer if they would like to add more items,
+                    # deliver the order or cancel the order
                     choice = input(
                         "Would you like to purchase this order and deliver it, add more items, or cancel the order?: ")
                     print()
+
+                    # If the user entered input
                     if len(choice) != 0:
+
+                        # Process fuzzy logic on the acceptable options
                         (selectedChoice, selectedChoiceConfidence) = process.extractOne(
-                            choice, ["purchase", "add items", "cancel"])
+                            choice, ["deliver", "add items", "cancel"])
+
+                        # If fuzzy logic is more than 80% confident
                         if selectedChoiceConfidence >= 80:
-                            if selectedChoice == "purchase":
+
+                            # User wants to deliver the order
+                            if selectedChoice == "deliver":
                                 print("Thanks for ordering!")
+
+                                # Add the order for the customer on file
                                 customer.addOrder(customerOrder)
                                 print("We have saved your order to the file.")
-                                goingtoConfirm = False
+
+                                # Exit the loop
+                                break
+
+                            # Customer chooses to add more items to the order
                             elif selectedChoice == "add items":
                                 print(
                                     "Alright, let's add some more items to your order.")
+
+                                # Recurse back to the ordering stage with the
+                                # current order
                                 return orderingStage(customer, customerOrder)
+
+                            # CUstomer wishes to cancel the order
                             elif selectedChoice == "cancel":
                                 print("Alright, we're cancelling this order.")
+
+                                # Exit the loop
                                 break
+
+                # Not enough items in the order to complete a delivery
                 else:
                     print(
                         "Unfortunately, you don't have enough items on your order, so we can't deliver this order yet.")
-                    willContinue = True
-                    while willContinue:
+                    while True:
+
+                        # User input to continue adding items or to cancel
+                        # the current order
                         continuationInput = input(
                             "Would you like to continue with your order, or cancel the order?: ")
+
+                        # Fuzzy process the user input to continue with the
+                        # order or to cancel the order
                         (continuationChoice, continuationConfidence) = process.extractOne(
                             continuationInput, ["continue with order", "cancel"])
+
                         if continuationConfidence >= 80:
+
+                            # Customer chooses to continue with the order
                             if continuationChoice == "continue with order":
-                                willContinue = False
+                                # Recurse back to the ordering stage
                                 return orderingStage(customer, customerOrder)
+
+                            # Customer chooses to cancel the order
                             elif continuationChoice == "cancel":
                                 print()
                                 print()
                                 print("No worries, we're cancelling this order.")
                                 print()
-                                willContinue = False
-                    goingtoConfirm = False
+
+                                # Exit out of the loop
+                                break
 
             return
